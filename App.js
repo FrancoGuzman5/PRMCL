9edcc124d9e77cca5b1c4b5ca1e4a9b5498e340b
Nato <natogxzman@gmail.com>
2022-05-04 00:45:53 -0400

    Pantalla de inicio app e icono, elegir una imagen y opcion compartir

--- App.js
+++ App.js
@@ -1,10 +1,65 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import { Text, View, StyleSheet, TouchableOpacity, Alert, Image } from "react-native";
+import * as ImagePicker from 'expo-image-picker'
+import * as Sharing from 'expo-sharing'
 
 const App = () => {
+
+  const [selectedImage, setSelectedImage] = useState(null)
+  
+  let openImagePickerAsync = async () => {
+    let resultadoPermiso = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (resultadoPermiso.granted == false){
+      alert('Permiso para acceder a la cámara es requerido');
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync()
+    if (pickerResult.cancelled === true){
+      return;
+    }
+
+    setSelectedImage({localUri: pickerResult.uri});
+
+  }
+
+  const openShareDialog = async() => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert('Compartir, no esta disponible en tu plataforma')
+    return;
+    }
+    await Sharing.shareAsync(selectedImage.localUri);
+  }
+
+
+  
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> Despliegue presentación avance.....</Text>
+      <Text> Despliegue presentación avance.....</Text>
+      <TouchableOpacity
+      onPress={openImagePickerAsync}>
+      <Image 
+        source={{
+          uri:
+            selectedImage !== null
+            ? selectedImage.localUri
+            : 'https://picsum.photos/200/200'
+          }}
+        style={styles.image}  
+      /> 
+      </TouchableOpacity> 
+      {
+        selectedImage ?
+        <TouchableOpacity onPress={openShareDialog}
+      style={styles.button}
+      >
+         <Text style={styles.buttonText}>
+           Compartir!</Text>
+      </TouchableOpacity>
+      :
+      <View/>
+      }
     </View>
   );
 };
@@ -13,11 +68,25 @@ const styles = StyleSheet.create({
   container: { 
       flex: 1,
       justifyContent: "center", 
-      alignItems: "center" 
+      alignItems: "center",
+      backgroundColor: 'lightgray',  
     },
+  image: {
+    height: 200, 
+    width: 200,
+    borderRadius: 100,
+    resizeMode: 'contain'
+  },
+  button: {
+    backgroundColor: 'lightgreen',
+    padding: 7,
+    marginTop: 10
+  },
+  buttonText: {
+    color: 'darkgreen'
+    
 
-  title: { 
-      fontsize: 30 
-    },
+  }
+
 });
 export default App;
