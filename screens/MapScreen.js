ba76a718530e6d0ccffa5fa46ca2f0031701a115
Nato <natogxzman@gmail.com>
2022-06-08 02:17:40 -0400

    Implementación google maps

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => { 
     return (
         <View>
-            <Text>Vista de mapa</Text>
+            <MapView style={styles.map}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    map: {
+        width: '100%',
+        height: '100%'
+    }
+})
 
 export default MapScreen
